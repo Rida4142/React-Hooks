a9f777e29552933c6263ba22c216817b1b270de6
Rida4142 <waheedrida56@gmail.com>
2025-08-29 07:17:05 +0100

    useState with Array

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import UseState from './components/UseState';
 import Counter from './components/Counter';
 import UseStateObject from './components/UseStateObject';
+import UseStateArray from './components/UseStateArray';
 
 function App() {
   return (
     <div className="App">
-      <UseStateObject />
+      <UseStateArray />
+      {/* <UseStateObject /> */}
       {/* <Counter /> */}
       {/* <UseState /> */}
     </div>
---
+++ src/components/UseStateArray.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+function UseStateArray() {
+    const [items,setItems]=useState([])
+    const addHandler=()=>{
+        setItems([...items,{
+             id: items.length,
+            value: Math.floor(Math.random()* 100)+1
+        }
+           
+        ])
+    }
+  return (
+    <div>
+        <button onClick={addHandler}>Add random number</button>
+        <ul>
+            {
+             items.map(item=>(
+                <li key={item.id}>{item.value}</li>
+              
+             ))
+            }
+            
+        </ul>
+     
+        
+      
+    </div>
+  )
+}
+
+export default UseStateArray
